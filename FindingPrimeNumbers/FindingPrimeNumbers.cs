186279e5431f39082f852fe5e91972858c228825
Sijie-Liu <liu.sij@husky.neu.edu>
2015-09-21 10:18:12 -0400

    Prime Numbers without cleaning stylecop warnings.

--- FindingPrimeNumbers/FindingPrimeNumbers.cs
+++ FindingPrimeNumbers/FindingPrimeNumbers.cs
@@ -1,14 +1,42 @@
-﻿namespace FindingPrimeNumbers
+﻿using System;
+
+namespace FindingPrimeNumbers
 {
 	public class FindingPrimeNumbers
 	{
 		public long FindSumOfPrimeNumbers(int n)
 		{
 			long sum = 0;
+            if (n == 0)
+                return 0;
+            if (n == 1)
+                return 2;
 
+            sum = 2;
+            int count = 1;
+            long temp = 0;
+            for (int i = 3;count<n ; i++)
+            {
+                if ((temp = isPrime(i)) != -1)
+                {
+                    sum += temp;
+                    count++;
+                }
+
+            }
 			// your solution goes in here
 
 			return sum;
 		}
+
+        public long isPrime(long num)
+        {
+            for (int i = 2; i <= Math.Sqrt(num); i++)
+            {
+                if (num % i == 0)
+                    return -1;
+            }
+            return num;
+        }
 	}
 }

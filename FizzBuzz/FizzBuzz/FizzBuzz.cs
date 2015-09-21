c51e1057ba4fd24487ed60ab85377b638b9719fc
Sijie-Liu <liu.sij@husky.neu.edu>
2015-09-21 10:51:05 -0400

    FizzBuzz without cleaning warnings.

--- FizzBuzz/FizzBuzz/FizzBuzz.cs
+++ FizzBuzz/FizzBuzz/FizzBuzz.cs
@@ -6,9 +6,27 @@
 		{
 			string result = number.ToString();
 
-			// your solution/implementation should go in here
+            // your solution/implementation should go in here
+            
+            string fb = "FizzBuzz";
+            bool flag3 = false;
+            bool flag5 = false;
+            if (number == 0)
+                return "0";
 
-			return result;
+            if (number % 5 == 0)
+                flag5 = true;
+            if (number % 3 == 0)
+                flag3 = true;
+
+            if (flag3 && flag5)
+                return fb;
+            else if (flag3 && !flag5)
+                return "Fizz";
+            else if (flag5 && !flag3)
+                return "Buzz";
+            else
+			    return result;
 		}
 	}
 }

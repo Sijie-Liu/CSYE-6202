a02cd397c8bca8b0c5bb723f7ed52def28c9f505
Sijie-Liu <liu.sij@husky.neu.edu>
2015-09-30 11:57:19 -0400

    Assignment5 final version

--- GasPump/GasPump/Program.cs
+++ GasPump/GasPump/Program.cs
@@ -23,9 +23,12 @@ namespace GasPump
 		{
 			var result = false;
 
-			// your implementation here
-
-			return result;
+            // your implementation here
+            if (userInput == null)
+                return false;
+            if (userInput.Equals("r") || userInput.Equals("m") || userInput.Equals("p") || userInput.Equals("d")|| userInput.Equals("q")|| userInput.Equals("Q"))
+                result = true;
+            return result;
 		}
 
 		// use this method to parse and check the characters entered
@@ -34,7 +37,12 @@ namespace GasPump
 		{
 			var result = false;
 
-			// your implementation here
+            if (userInput == null)
+                return false;
+            // your implementation here
+            string req = userInput.ToLower();
+            if(req.Equals("r")|| req.Equals("m")|| req.Equals("p")|| req.Equals("d"))
+                result = true;
 			
 			return result;
 		}
@@ -45,7 +53,23 @@ namespace GasPump
 		{
 			var result = false;
 
-			// your implementation here
+            if (userInput == null)
+                return false;
+
+            // your implementation here
+            char[] arr = userInput.ToCharArray();
+            for (int i=0;i<arr.Length;i++)
+            {
+                if ((arr[i] <= '9' && arr[i] >= '0') || arr[i] == '.')
+                {
+                    result = true;
+                }
+                else
+                {
+                    result = false;
+                    break;
+                }
+            }
 
 			return result;
 		}
@@ -57,7 +81,34 @@ namespace GasPump
 		{
 			GasType gasType = GasType.None;
 
-			// your implementation here
+            // your implementation here
+            switch (c)
+            {
+                case 'r':
+                    gasType = GasType.RegularGas;
+                    break;
+                case 'R':
+                    gasType = GasType.RegularGas;
+                    break;
+                case 'p':
+                    gasType = GasType.PremiumGas;
+                    break;
+                case 'P':
+                    gasType = GasType.PremiumGas;
+                    break;
+                case 'd':
+                    gasType = GasType.DieselFuel;
+                    break;
+                case 'D':
+                    gasType = GasType.DieselFuel;
+                    break;
+                case 'm':
+                    gasType = GasType.MidgradeGas;
+                    break;
+                case 'M':
+                    gasType = GasType.MidgradeGas;
+                    break;
+            }
 
 			return gasType;
 		}
@@ -65,7 +116,21 @@ namespace GasPump
 		public static double GasPriceMapper(GasType gasType)
 		{
 			var result = 0.0;
-
+            switch (gasType)
+            {
+                case GasType.RegularGas:
+                    result = 1.94;
+                    break;
+                case GasType.MidgradeGas:
+                    result = 2.0;
+                    break;
+                case GasType.PremiumGas:
+                    result = 2.24;
+                    break;
+                case GasType.DieselFuel:
+                    result = 2.17;
+                    break;
+            }
 			// your implementation here
 
 			return result;
@@ -73,7 +138,8 @@ namespace GasPump
 
 		public static void CalculateTotalCost(GasType gasType, int gasAmount, ref double totalCost)
 		{
-			// your implementation here
+            // your implementation here
+            totalCost = GasPriceMapper(gasType) * gasAmount;
 		}
 	}
 }

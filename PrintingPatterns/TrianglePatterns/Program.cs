ceac9bf812a30b4d19663b5e27687be5ecb431cd
Sijie-Liu <liu.sij@husky.neu.edu>
2015-09-30 10:58:01 -0400

    Assignment4_Final_Version

--- PrintingPatterns/TrianglePatterns/Program.cs
+++ PrintingPatterns/TrianglePatterns/Program.cs
@@ -16,22 +16,72 @@ namespace TrianglePatterns
 
 		static void DisplayPatternA()
 		{
-			// your implementation here
-		}
+            // your implementation here
+            Console.WriteLine("Pattern A:");
+            for (int i = 1; i <= 10; i++)
+            {
+                for (int j = 1; j <= i; j++)
+                    Console.Write("#");
+                Console.WriteLine();
+            }
+            Console.WriteLine();
+        }
 
 		static void DisplayPatternB()
 		{
-			// your implementation here
-		}
+            // your implementation here
+            Console.WriteLine("Pattern B:");
+            for (int i = 10; i >= 1; i--)
+            {
+                for (int j = 1; j <= i; j++)
+                    Console.Write("#");
+                Console.WriteLine();
+            }
+            Console.WriteLine();
+        }
 
 		static void DisplayPatternC()
 		{
-			// your implementation here
-		}
+            // your implementation here
+            Console.WriteLine("Pattern C:");
+            for (int i = 10; i >= 1; i--)
+            {
+                int j = 10;
+                while (j - i > 0)
+                {
+                    Console.Write(" ");
+                    j--;
+                }
+                while (j >= 1)
+                {
+                    Console.Write("#");
+                    j--;
+                }
+                Console.WriteLine();
+            }
+            Console.WriteLine();
+        }
 
 		static void DisplayPatternD()
 		{
-			// your implementation here
-		}
+            // your implementation here
+            Console.WriteLine("Pattern D:");
+            for (int i = 10; i >= 1; i--)
+            {
+                int j = 1;
+                while (i - j > 0)
+                {
+                    Console.Write(" ");
+                    j++;
+                }
+                while (j <= 10)
+                {
+                    Console.Write("#");
+                    j++;
+                }
+                Console.WriteLine();
+            }
+            Console.WriteLine();
+        }
 	}
 }
